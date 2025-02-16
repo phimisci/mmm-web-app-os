6f27d6d2773d9b89507e717d067ce2200d95ab0e
pyrist <pyristfeuer@gmail.com>
2025-02-16 15:58:18 +0100

    changed SI teaser label

--- mmm/maker_project/forms.py
+++ mmm/maker_project/forms.py
@@ -37,7 +37,7 @@ class MMMDynamicForm(FlaskForm):
     orcids = StringField('ORCIDs')
     year = StringField('Year')
     doi = StringField('DOI')
-    special_issue = StringField('SpecialIssue')
+    special_issue = StringField('Special Issue Teaser')
     submit = SubmitField('Create files')
     # Additional information for DOC2MD:
     zotero_used = BooleanField('Zotero used')
